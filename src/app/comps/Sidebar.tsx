6e9ada7f66d43916afe3e4b04cc4fe7c43ceab9f
IVSN (Ivan Serrano) <IVSN@novonordisk.com>
2025-09-12 14:07:15 +0200

    sidebar correct links

--- src/app/comps/Sidebar.tsx
+++ src/app/comps/Sidebar.tsx
@@ -1,56 +1,52 @@
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-} from '@/components/ui/sidebar';
-import { useContext } from 'react';
-import { LanguageContext, Language } from './context';
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { useContext } from "react";
+import { LanguageContext, Language } from "./context";
 
 // Menu items.
 const items = [
-	{
-		title: { ES: 'Equipo', CAT: 'Equip' },
-		url: '/Quiénes somos',
-	},
-	{
-		title: { ES: 'Servicios', CAT: 'Serveis' },
-		url: '/servicios',
-	},
-	{
-		title: { ES: 'FAQ', CAT: 'FAQ' },
-		url: '/faq',
-	},
-	{
-		title: { ES: 'Contacto', CAT: 'Contacte' },
-		url: '/contact',
-	},
+  {
+    title: { ES: "Quiénes somos", CAT: "Qui som" },
+    url: "/Quiénes somos",
+  },
+  {
+    title: { ES: "Servicios", CAT: "Serveis" },
+    url: "/servicios",
+  },
+  {
+    title: { ES: "Contacto", CAT: "Contacte" },
+    url: "/contact",
+  },
 ];
 
 export function AppSidebar() {
-	const { language } = useContext(LanguageContext);
-	return (
-		<Sidebar>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarGroupContent>
-						<SidebarMenu>
-							{items.map((item) => (
-								<SidebarMenuItem key={item.title[language as Language]}>
-									<SidebarMenuButton asChild>
-										<a href={item.url}>
-											<span>{item.title[language as Language]}</span>
-										</a>
-									</SidebarMenuButton>
-								</SidebarMenuItem>
-							))}
-						</SidebarMenu>
-					</SidebarGroupContent>
-				</SidebarGroup>
-			</SidebarContent>
-		</Sidebar>
-	);
+  const { language } = useContext(LanguageContext);
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title[language as Language]}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <span>{item.title[language as Language]}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
