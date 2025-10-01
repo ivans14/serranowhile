adc3769a9aa192dde64bc87cb1799166c2d45540
ivanEnsenya <ssivon14@gmail.com>
2025-10-01 13:49:40 +0200

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,32 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "./comps/Navbar";
-import Footer from "./comps/Footer";
-import ContextProvider from "./comps/context";
+import type { Metadata } from 'next';
+import './globals.css';
+import Navbar from './comps/Navbar';
+import Footer from './comps/Footer';
+import ContextProvider from './comps/context';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
-  title: "Serrano While",
-  description:
-    "Soluciones profesionales integrales para empresas, directivos y particulares. Consultoría, fiscalidad, procesos y estrategia",
+	title: 'Serrano While',
+	description:
+		'Soluciones profesionales integrales para empresas, directivos y particulares. Consultoría, fiscalidad, procesos y estrategia',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`antialiased flex items-center flex-col min-h-[100vh]`}>
-        <ContextProvider>
-          <Navbar />
-          {children}
+	return (
+		<html lang='en'>
+			<body className={`antialiased flex items-center flex-col min-h-[100vh]`}>
+				<ContextProvider>
+					<Navbar />
+					{children}
+					<Analytics />
 
-          <Footer />
-        </ContextProvider>
-      </body>
-    </html>
-  );
+					<Footer />
+				</ContextProvider>
+			</body>
+		</html>
+	);
 }
